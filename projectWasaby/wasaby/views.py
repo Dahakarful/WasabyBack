b5a415afee9797c9256abf60eb509c175b1a5566
Dahakarful <tsar4le4grand@gmail.com>
2017-05-11 15:19:50 +0200

    chercher toute les bouées

--- projectWasaby/wasaby/views.py
+++ projectWasaby/wasaby/views.py
@@ -1,15 +1,23 @@
 import json
-import requests
 import elasticsearch
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
+from requests_aws4auth import AWS4Auth
+from django.http import JsonResponse
 
-url = 'https://search-elasticwasaby-y2o2s4be7d3uiwq3tr6jrkfr7i.eu-west-2.es.amazonaws.com/'
 indexName = 'wasaby'
 typeName = 'buoys'
 jsonPath = '../resources/result.json'
 
-es = elasticsearch.Elasticsearch()
+host = 'search-elasticwasaby-y2o2s4be7d3uiwq3tr6jrkfr7i.eu-west-2.es.amazonaws.com'
+awsauth = AWS4Auth('AKIAJVGUUFRVMOJVBEIA', 'p9MPwHHMg+15LRbTgXcenreY5n+ikb3i9LgHv+Wc', 'eu-west-2', 'es')
+
+es = elasticsearch.Elasticsearch(
+    hosts=[{'host': host, 'port':443}],
+    http_auth=awsauth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=elasticsearch.RequestsHttpConnection
+)
 
 def indexBuoys(request):
     print(request)
@@ -21,8 +29,9 @@ def indexBuoys(request):
         print(res)
     return HttpResponse("Number of created objects %d" % len(nodes))
 
-
-@csrf_exempt
-def searchABuoy(self):
-    data = self.request.data
-    print(data)
+def searchABuoy(request):
+    print(request)
+    res = es.search(index=indexName, filter_path=['hits.hits._source.*'], body={"query": {"match_all": {}}})
+    print(res)
+    jsonObj = json.dumps(res)
+    return JsonResponse(jsonObj, safe=False)
