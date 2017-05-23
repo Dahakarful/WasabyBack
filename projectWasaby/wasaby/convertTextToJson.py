834672a6624d699b5d703308f360e1538ae1957b
Dahakarful <tsar4le4grand@gmail.com>
2017-05-23 14:53:55 +0200

    fichier bouées + conversion en fichier JSON

--- projectWasaby/wasaby/convertTextToJson.py
+++ projectWasaby/wasaby/convertTextToJson.py
@@ -2,23 +2,15 @@ import csv
 import json
 import os
 
-#convert txt to csv
+# convert txt to csv
 filein = open('../resources/ndbc_listing.txt','r')
 fileout = open('../resources/temp.txt','w')
 
 for line in filein:
-    a = line.replace('Lat','').replace('.nc', '').replace('_spectrum', '')
-    b = a.replace('Lon','')
-    c= b.replace('/',',')
-    d = c.replace('_',',')
-    e = d.replace(',,',',')
-    f = e.replace('.','', 1)
-    g = f.replace(',','', 1)
-    if(g[4]==','):
-        h=g.replace(',','', 1)
-        fileout.write(h)
-    else:
-        fileout.write(g)
+    a = line.replace('_', ',').replace('./', '').replace('/', ',').replace('Lat', '').replace('Lon', '').replace('.nc', '')
+    b = a.replace('spectrum', '').replace('sensor', '').replace(',,', ',').replace(' ', '')
+    g = b.replace('ocean,temperature', 'ocean temperature').replace('air,temperature', 'air temperature')
+    fileout.write(g)
     print(g)
 filein.close()
 fileout.close()
@@ -31,3 +23,4 @@ fieldnames=['type','year','month','id','start','end','lat','lon']
 
 reader = csv.DictReader(csvfile,fieldnames)
 json.dump([row for row in reader],jsonfile)
+print('Done!')
