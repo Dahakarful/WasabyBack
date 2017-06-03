c8dcc55a63ff281736e9d6951967f055ae554cb3
Dahakarful <tsar4le4grand@gmail.com>
2017-06-03 15:48:19 +0200

    correction cors headers

--- projectWasaby/projectWasaby/settings.py
+++ projectWasaby/projectWasaby/settings.py
@@ -59,6 +59,25 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     'http://wasaby-front.cleverapps.io/'
 )
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
 
 TEMPLATES = [
     {
